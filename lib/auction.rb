140a4974b9e8247237ca7d78b0b7b4be8b182eab
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-02 13:42:48 -0700

    Add test suite and class for auction

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,2 +1,17 @@
+
+
 class Auction
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    # item_params = {
+    #   name: item.name
+    # }
+    # item = Item.new(item_params)
+    @items << item
+  end
 end
--- test/auction_test.rb
+++ test/auction_test.rb
@@ -1,9 +1,28 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/auction'
+require './lib/item'
 
 class AuctionTest < Minitest::Test
   def setup
     @auction = Auction.new
+  end
+
+  def test_it_exists
+    assert_instance_of Auction, @auction
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal [], @auction.items
+  end
+
+  def test_it_can_add_items
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    @auction.add_item(item1)
+    @auction.add_item(item2)
+    # require 'pry'; binding.pry
+    assert_equal [item1, item2], @auction.items
   end
 end
