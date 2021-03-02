fa036c06dab93773d4990dba283a20de8a592f68
Carlyle Douglas <cdouglascorp@gmail.com>
2021-03-02 13:22:15 -0700

    Add test suite for Item class

--- test/item_test.rb
+++ test/item_test.rb
@@ -7,4 +7,14 @@ class ItemTest < Minitest::Test
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
   end
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+    assert_instance_of Item, @item2
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal @item1.name, 'Chalkware Piggy Bank'
+    assert_equal @item2.name, 'Bamboo Picture Frame'
+  end
 end
